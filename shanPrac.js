5953d7bc1abad5d07fbdab233958550b4f47c748
zeeshanghani <mzeeshanghani786@gmail.com>
2023-08-21 20:44:22 -0700

    my new file

---
+++ shanPrac.js
@@ -0,0 +1,5 @@
+"use strict";
+var num = 3;
+var num2 = 8;
+var result = num + num2;
+console.log("sum=", result);
---
+++ shanPrac.ts
@@ -0,0 +1,4 @@
+var num:number=3
+var num2:number=8
+var result:number=num+num2
+console.log("sum=",result)
